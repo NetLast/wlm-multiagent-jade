0f671796e37f019aacb6a8d7df34aa8245874939
NetLast <111975038+NetLast@users.noreply.github.com>
2026-05-17 23:48:25 +0300

    Create TechValidatorAgent.java

---
+++ src/main/java/ua/nure/lastovets/wlm/TechValidatorAgent.java
@@ -0,0 +1,68 @@
+package ua.nure.lastovets.wlm;
+
+import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+import jade.proto.ContractNetResponder;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.util.Base64;
+
+public class TechValidatorAgent extends Agent {
+
+    protected void setup() {
+        System.out.println("🔧 TechValidatorAgent " + getAID().getLocalName() + " запущено.");
+        registerInDF();
+
+        addBehaviour(new TechValidationResponder());
+    }
+
+    private void registerInDF() {
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("tech-validator");
+        sd.setName("Technical-Photo-Validator");
+        dfd.addServices(sd);
+        try { DFService.register(this, dfd); } catch (FIPAException e) { e.printStackTrace(); }
+    }
+
+    private class TechValidationResponder extends ContractNetResponder {
+        public TechValidationResponder() {
+            super(myAgent, null);
+        }
+
+        protected ACLMessage handleCfp(ACLMessage cfp) {
+            ACLMessage propose = cfp.createReply();
+            propose.setPerformative(ACLMessage.PROPOSE);
+            propose.setContent("ready|" + System.currentTimeMillis());
+            return propose;
+        }
+
+        protected ACLMessage handleAcceptProposal(ACLMessage cfp, ACLMessage propose, ACLMessage accept) {
+            ACLMessage inform = accept.createReply();
+            inform.setPerformative(ACLMessage.INFORM);
+
+            try {
+                // Симуляція перевірки зображення
+                Thread.sleep(800); // імітація обробки
+                inform.setContent("TECH_OK: " + cfp.getContent());
+                System.out.println("✅ " + getAID().getLocalName() + " пройшов технічну перевірку");
+            } catch (Exception e) {
+                inform.setPerformative(ACLMessage.FAILURE);
+                inform.setContent("TECH_FAILED");
+            }
+            return inform;
+        }
+    }
+
+    protected void takeDown() {
+        try { DFService.deregister(this); } catch (Exception ignored) {}
+    }
+}
